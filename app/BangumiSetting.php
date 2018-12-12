63eff2dc09f17ee9e1a8939ff3256c57b1b8b450
Tsukasa <tsukasa.kzk@gmail.com>
2018-12-13 06:30:15 +0800

    feat: 同步账户修改

--- app/BangumiSetting.php
+++ app/BangumiSetting.php
@@ -2,9 +2,46 @@
 
 namespace App;
 
+use Illuminate\Contracts\Encryption\Encrypter;
 use Illuminate\Database\Eloquent\Model;
 
 class BangumiSetting extends Model
 {
     //
+    protected $fillable = [
+        'sitedriver',
+        'username',
+        'password',
+        'status',
+    ];
+
+    private $avaliableSites = [
+        'AcgRip'     => 'ACG.RIP',
+        'MoeBangumi' => '萌番组',
+        'Dmhy'       => '动漫花园',
+        'Nyaa'       => 'Nyaa',
+        'Kisssub'    => '爱恋动漫BT下载',
+    ];
+
+    public function setPasswordAttribute($value)
+    {
+        if ($value) {
+            $this->attributes['password'] = app()->make(Encrypter::class)->encrypt($value, false);
+        }
+    }
+
+    public function getPasswordAttribute($value)
+    {
+        return app()->make(Encrypter::class)->decrypt($value, false);
+    }
+
+    public function setSitedriverAttribute($value)
+    {
+        $avaliableDriver = array_keys($this->avaliableSites);
+        if (in_array($value, $avaliableDriver)) {
+            $this->attributes['sitedriver'] = $value;
+            $this->attributes['sitename']   = $this->avaliableSites[$value];
+        }
+    }
+
 }
--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\BangumiSetting;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class SettingController extends Controller
 {
@@ -19,6 +20,19 @@ class SettingController extends Controller
 
     public function updateBangumiSettings(Request $request, $id)
     {
+        $setting = BangumiSetting::find($id);
+        if (is_null($setting)) {
+            Log::info('找不到同步账户', ['id' => $id, 'action' => 'updateBangumiSettings']);
+            abort(404, 'Settings not found');
+        }
+        $request->validate([
+            'sitedriver' => 'required',
+            'username'   => 'required',
+            'status'     => 'required',
+        ]);
 
+        $setting->fill($request->toArray());
+        $setting->save();
+        return response([], 200);
     }
 }
